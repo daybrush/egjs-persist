9f5f0ddc23e2cc4d240793ae02d926cfcb5d6b77
Daybrush <younkue.choi@navercorp.com>
2019-01-29 16:41:20 +0900

    Updates

--- test/js/test.js
+++ test/js/test.js
@@ -6,8 +6,11 @@ var depths = (eg.Persist.StorageManager.getStateByKey("state___persist___", "dep
 
 	return dirs[dirs.length - 1];
 }).join(" -> ");
+
+var type = typeof performance !== "undefined" && performance.navigation && performance.navigation.type;
 document.querySelector("#testlog").innerHTML =
 	"isPersistNeeded : " + isPersistNeeded + "<br/>" + 
 	"length : " + depths + "<br/>" + 
 	"last : " + eg.Persist.StorageManager.getStateByKey("state___persist___", "lastUrl") + "<br/>" + 
+	"type : " + type  + "<br/>" + 
 	"";
